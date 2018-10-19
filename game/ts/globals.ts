32b680d9fcf871638d845bd4f2b10710e786f2f2
Dave <imdaveead@gmail.com>
2018-10-19 17:43:11 -0400

    work on loading stuff

--- game/ts/globals.ts
+++ game/ts/globals.ts
@@ -1,5 +1,7 @@
 /** Sets a global variable to stuff. */
 export function exposeGlobals() {
+    return;
+
     window["$ts"] = {
         api: require("./api-interface"),
         elem: require("./elem-ui"),
